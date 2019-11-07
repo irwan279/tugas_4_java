65abc5f7953ea25e8e2f668d94b52e63de95afb9
Irwan M Faizal <irwan279217@gmail.com>
2019-11-07 08:49:24 +0700

    Tugas 4 java
    Niomic

---
+++ tugas4javascript.js
@@ -0,0 +1,11 @@
+var angka1 = 10;
+var angka2 = 20;
+var angka3 = 30;
+
+if (angka1>angka2 && angka1>angka3) {
+  console.log(angka1); }
+  else if (angka2>angka1 && angka2>angka3) {
+    console.log(angka2);}
+    else {
+      console.log(angka3);
+}
